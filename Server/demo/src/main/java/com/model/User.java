2b4a28d7a242a5f0e1caac1ec80f164e3afbf728
Chris Kalama <chriskalama96@gmail.com>
2021-07-21 00:07:58 -0500

    auto-sourced hashcode, equals, and toString methods from IDE

--- Server/demo/src/main/java/com/model/User.java
+++ Server/demo/src/main/java/com/model/User.java
@@ -1,7 +1,11 @@
 package com.model;
 
-//This is the model class, here is built a simple User who will get your static variables, generic Constructor
-// and getters/setters. 
+//This is the model class, here is built a simple User who will get your instance variables, generic Constructor
+// and getters/setters.
+
+//You will also need a Constructor with ALL instance variables
+//Auto-Source hashcode() and equals() methods
+//Auto-Source your toString() method.
 
 public class User {
 	
@@ -15,6 +19,17 @@ public class User {
 		super();
 	}
 	
+
+	public User(String firstName, String lastName, Integer age, String country) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.country = country;
+	}
+
+
+
 	public String getFirstName() {
 		return firstName;
 	}
@@ -46,4 +61,60 @@ public class User {
 	public void setCountry(String country) {
 		this.country=country;
 	}
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
+
+	@Override
+	public String toString() {
+		return "User [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", country=" + country
+				+ "]";
+	}
+	
+	
+	
+	
 }
