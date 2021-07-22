c397207dc56a53ba6764188ad0cbefc5c64808d7
Chris Kalama <chriskalama96@gmail.com>
2021-07-21 22:53:55 -0500

    created basic Service findAll method for Users. Also needed a ServiceImpl for UserService. auto-generated the implementation and added proper annotations to tell Spring this is where business logic goes for User. Then added notes and basic return on findAll method

---
+++ Server/demo/src/main/java/com/service/impl/UserServiceImpl.java
@@ -0,0 +1,30 @@
+package com.service.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.model.User;
+import com.service.UserService;
+
+//This needs to implement the User Service
+//We need to tell Spring this is a ServiceImpl Class with the @Service annotation 
+	//This tells Spring that under this class we will have business logic 
+	//Business logic of the Service class goes under the ServiceImpl class
+
+@Service
+public class UserServiceImpl implements UserService{
+
+	
+	//This is the serviceImpl method that will find all Users. It was instantiated on the UserService interface.
+	
+	@Override
+	public List<User> findAll() {
+		// TODO Auto-generated method stub
+		List<User> usersList = new ArrayList<>();
+		return usersList;
+	}
+	
+
+}
