4b8520f2ff23176ea5623887059dc7ad9a691f79
Chris Kalama <chriskalama96@gmail.com>
2021-08-06 18:30:04 -0500

    Configured MDBootstrap as the CSS template I will use. npm installed the packages, configured the app-component, angular.json, and app.module to host MDBootsrap. Created basic structure of app. Generated a user component and a user services ts file

--- Client/app/src/app/app.module.ts
+++ Client/app/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './components/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
