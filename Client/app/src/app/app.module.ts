df43b3bd054aae9bf83d8cd16515b54b91ac442c
Chris Kalama <chriskalama96@gmail.com>
2021-08-16 12:54:38 -0500

    created initial files for authentication. Added a services authentcation and a constants.ts file so that we can access the backend server and authenticate our login whenever we fire up the backend

--- Client/app/src/app/app.module.ts
+++ Client/app/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserComponent } from './components/user/user.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,8 @@ import { UserComponent } from './components/user/user.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
     MDBBootstrapModule.forRoot()
   ],
   providers: [],
